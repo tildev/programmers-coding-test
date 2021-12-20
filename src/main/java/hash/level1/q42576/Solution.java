6745be36842077cb631605d2d9654751eac74a4e
tildev <tildevelove@gmail.com>
2021-12-20 23:08:36 +0900

    Solve the problem 42576 of hash-lv1

---
+++ src/main/java/hash/level1/q42576/Solution.java
@@ -0,0 +1,24 @@
+package hash.level1.q42576;
+
+import java.util.Arrays;
+
+/**
+ * 완주하지 못한 선수
+ *
+ * @author tildev
+ * date 2021-12-20
+ */
+public class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        int i;
+        for (i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i])) {
+                return participant[i];
+            }
+        }
+        return participant[i];
+    }
+}
