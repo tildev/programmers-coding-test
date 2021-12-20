103bb09564707e09040b5beb2e67b39fd6a6bc5f
tildev <tildevelove@gmail.com>
2021-12-20 23:09:13 +0900

    Add test - 42576 of hash-lv1

---
+++ src/test/java/Hash.java
@@ -0,0 +1,25 @@
+import hash.level1.q42576.Solution;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Hash {
+    @Test
+    public void q42576(){
+        Solution solution = new Solution();
+
+        String[] participant1 = {"leo", "kiki", "eden"};
+        String[] completion1 = {"eden", "kiki"};
+        String answer1 = solution.solution(participant1, completion1);
+        Assertions.assertEquals("leo", answer1);
+
+        String[] participant2 = {"marina", "josipa", "nikola", "vinko", "filipa"};
+        String[] completion2 = {"josipa", "filipa", "marina", "nikola"};
+        String answer2 = solution.solution(participant2, completion2);
+        Assertions.assertEquals("vinko", answer2);
+
+        String[] participant3 = {"mislav", "stanko", "mislav", "ana"};
+        String[] completion3 = {"stanko", "ana", "mislav"};
+        String answer3 = solution.solution(participant3, completion3);
+        Assertions.assertEquals("mislav", answer3);
+    }
+}
